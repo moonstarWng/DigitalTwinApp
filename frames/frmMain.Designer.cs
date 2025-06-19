bc196adf3bcaa7daec533f4b04547b6b9fea61c6
MoonstarWng <moonstarwng@163.com>
2025-06-19 21:55:18 +0800

    数字孪生软件MVP

---
+++ frames/frmMain.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace DigitalTwinApp
+{
+    partial class frmMain
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            wvMain = new Microsoft.Web.WebView2.WinForms.WebView2();
+            ((System.ComponentModel.ISupportInitialize)wvMain).BeginInit();
+            SuspendLayout();
+            // 
+            // wvMain
+            // 
+            wvMain.AllowExternalDrop = true;
+            wvMain.CreationProperties = null;
+            wvMain.DefaultBackgroundColor = Color.White;
+            wvMain.Dock = DockStyle.Fill;
+            wvMain.Location = new Point(0, 0);
+            wvMain.Name = "wvMain";
+            wvMain.Size = new Size(800, 450);
+            wvMain.TabIndex = 0;
+            wvMain.ZoomFactor = 1D;
+            // 
+            // frmMain
+            // 
+            AutoScaleDimensions = new SizeF(7F, 17F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(wvMain);
+            Name = "frmMain";
+            Text = "数字孪生测试";
+            Load += frmMain_Load;
+            ((System.ComponentModel.ISupportInitialize)wvMain).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Microsoft.Web.WebView2.WinForms.WebView2 wvMain;
+    }
+}
---
+++ frames/frmMain.cs
@@ -0,0 +1,20 @@
+﻿namespace DigitalTwinApp
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private async void frmMain_Load(object sender, EventArgs e)
+        {
+            //string htmlPath = Path.Combine(Application.StartupPath, "Assets", "index.html");
+            //string uri = new Uri(htmlPath).AbsoluteUri;
+            string uri = "http://localhost:8000/Assets/index.html";
+            await wvMain.EnsureCoreWebView2Async(null);
+            wvMain.Source = new Uri(uri);
+            wvMain.CoreWebView2.OpenDevToolsWindow();   // 打开控制台
+        }
+    }
+}
